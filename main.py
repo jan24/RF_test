4acae4dc0a818d1cc41e53579424948829bc7bfd
shu <1124908856@qq.com>
2019-01-14 06:34:42 +0800

    README.md

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+#python3.6
+class Rf_fix:
+    def __init__(self, name, testname, robotname):
+        #like cell45
+        self.name = name
+        #like rf42w7cell1 ,it associate with test cell name or connections with product console
+        self.testname = name
+        #like fix1, it associate with the position
+        self.robotname = robotname
+    
